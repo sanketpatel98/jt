48cb4d910ece2b6f90d161ac8b6a3c13323656d8
sanketpatel98 <48122030+sanketpatel98@users.noreply.github.com>
2019-04-05 19:58:15 +0530

    Add files via upload

---
+++ bn.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class bn {
+	private String name;
+	private String pass;
+	
+	
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
